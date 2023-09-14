53f187d0827b821beb2c59cca2fe0dc4b4e7531f
brkckr20 <burak2043@gmail.com>
2023-09-14 17:38:58 +0300

    camping-adonis started

---
+++ camping/start/routes.ts
@@ -0,0 +1,45 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer''
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async ({ view }) => {
+  return view.render('welcome', {
+    title : "home"
+  })
+})
+
+Route.get('/pages', async ({ view }) => {
+  return view.render('pages' , {
+    title : "pages"
+  })
+})
+
+Route.get('/gallery', async ({ view }) => {
+  return view.render('gallery' , {
+    title : "gallery"
+  })
+})
+
+Route.get('/blog', async ({ view }) => {
+  return view.render('blog' , {
+    title : "blog"
+  })
+})
